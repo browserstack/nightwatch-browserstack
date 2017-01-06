c00ed4df6629788b1c3ae6607eddaa3f98680e28
Brendan O'Rourke <orourkebm@gmail.com>
2017-01-05 16:36:09 -0800

    If set, add proxy settings to local runner

--- scripts/local.runner.js
+++ scripts/local.runner.js
@@ -2,15 +2,52 @@
 
 var Nightwatch = require('nightwatch');
 var browserstack = require('browserstack-local');
+var localConf = require('../conf/local.conf.js');
+
 var bs_local;
 
 try {
   process.mainModule.filename = "./node_modules/.bin/nightwatch"
 
   // Code to start browserstack local before start of test
-  console.log("Connecting local");
+  console.log('Connecting local');
   Nightwatch.bs_local = bs_local = new browserstack.Local();
-  bs_local.start({'key': process.env.BROWSERSTACK_ACCESS_KEY }, function(error) {
+
+  var localOptions = {
+    'key': process.env.BROWSERSTACK_ACCESS_KEY
+  };
+
+  /**
+   * If the network requires proxy configuration for outbound connections,
+   * set those here.
+   */
+  var proxySettings = localConf['proxy'];
+
+  for (var key in proxySettings) {
+    var value = proxySettings[key];
+
+    switch (key) {
+      case 'host':
+        if (value) localOptions['proxyHost'] = value;
+        break;
+
+      case 'port':
+        if (value) localOptions['proxyPort'] = value;
+        break;
+
+      case 'user':
+        if (value) localOptions['proxyUser'] = value;
+        break;
+
+      case 'pass':
+        if (value) localOptions['proxyPass'] = value;
+        break;
+    }
+  }
+
+  console.log('Local options: \n', localOptions);
+
+  bs_local.start(localOptions, function(error) {
     if (error) throw error;
 
     console.log('Connected. Now testing...');
